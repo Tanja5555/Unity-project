7b35da501ad1998dd184ea56fbb4009ccd6d1a16
Tetyana Leonova <tanja.v.leonova@gmail.com>
2020-05-18 11:13:25 +0200

    Skatter i vatten, av prefabs, instantieras vid start

---
+++ Unity-project/Assets/Scripts/IslandScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandScript : MonoBehaviour
+{
+    [SerializeField] private GameObject treasurePrefab;
+    private float yPos = 4.4f;
+    private int numberOfPrefabs = 60;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numberOfPrefabs; i++)
+        {
+            GameObject temp = Instantiate(treasurePrefab, transform);
+            float xPos = Random.Range(-80f, -100f);
+            float zPos = Random.Range(-20f, -40f);
+            temp.transform.position = new Vector3(xPos, yPos, zPos);
+        }
+    }
+}
