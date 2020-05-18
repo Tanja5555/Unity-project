8dfe0ba9d71d2e21f4ed6bba1719d20a761cd685
Tetyana Leonova <tanja.v.leonova@gmail.com>
2020-05-18 14:43:27 +0200

    aktivera och avaktivera pointlights i skatter med p-tangenten

---
+++ Unity-project/Assets/ChestScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestScript : MonoBehaviour
+{
+    [SerializeField] private Light[] lightObjects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var lightObject in lightObjects)
+        {
+            if(Input.GetKeyDown(KeyCode.P))
+            {
+                lightObject.enabled = !lightObject.enabled;
+            }
+        }
+    }
+}
