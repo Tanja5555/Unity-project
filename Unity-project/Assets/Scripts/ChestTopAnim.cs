b687e9701dbad5858d23603c272d4b94c48191cf
Tetyana Leonova <tanja.v.leonova@gmail.com>
2020-05-19 15:51:52 +0200

    animation för skattkista, behöver justeras

---
+++ Unity-project/Assets/Scripts/ChestTopAnim.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestTopAnim : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Collided with" + other.name);
+            anim.SetTrigger("OpenTop");
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Stopped collision with" + other.name);
+            anim.enabled = true;
+        }
+        
+    }
+
+    void PauseAnimationEvent()
+    {
+        Debug.Log("Waiting");
+        anim.enabled = false;
+    }
+}
