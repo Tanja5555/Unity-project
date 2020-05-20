d44d03c4016385bda8fe2589c313f69fc0f31bfa
Tetyana Leonova <tanja.v.leonova@gmail.com>
2020-05-20 16:53:31 +0200

    fallande kokosnötter med bounce, en till kista

---
+++ Unity-project/Assets/Scripts/CoconutFall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoconutFall : MonoBehaviour
+{
+    [HideInInspector] new public Rigidbody rb;
+    public bool useGravity = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        StartCoroutine(MakeCoconutFall());                
+    }
+
+    private IEnumerator MakeCoconutFall()
+    {
+        yield return new WaitForSecondsRealtime(3f);
+        rb.useGravity = false;
+
+        if (useGravity)
+        {
+            rb.AddForce(Physics.gravity * (rb.mass * rb.mass));
+        }
+
+    }
+}
