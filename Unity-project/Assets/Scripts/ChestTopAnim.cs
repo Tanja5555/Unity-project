3b8d0c43f2f602161cbce514bf8f969d57823107
Tetyana Leonova <tanja.v.leonova@gmail.com>
2020-05-20 11:42:26 +0200

    animation och ljud för skattkista

--- Unity-project/Assets/Scripts/ChestTopAnim.cs
+++ Unity-project/Assets/Scripts/ChestTopAnim.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class ChestTopAnim : MonoBehaviour
 {
     Animator anim;
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip audioClip;
+
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
-        {
-            Debug.Log("Collided with" + other.name);
+        {           
+            Debug.Log("Collided with" + other.name);           
             anim.SetTrigger("OpenTop");
+            StartCoroutine(PlaySound());
+            
         }
         
     }
@@ -24,16 +30,25 @@ public class ChestTopAnim : MonoBehaviour
     void OnTriggerExit(Collider other)
     {
         if(other.CompareTag("Player"))
-        {
+        {           
             Debug.Log("Stopped collision with" + other.name);
             anim.enabled = true;
-        }
-        
+            audioSource.PlayOneShot(audioClip);
+        }      
     }
 
     void PauseAnimationEvent()
     {
         Debug.Log("Waiting");
         anim.enabled = false;
+    }
+
+    private IEnumerator PlaySound()
+    {
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        yield return new WaitForSecondsRealtime(0.5f);
+        audioSource.PlayOneShot(audioClip);
+        Debug.Log("Stopped Coroutine at timestamp : " + Time.time);
     }
 }
