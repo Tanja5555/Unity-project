6df763822907037bdb03a8a900a58dc019a02c64
Tetyana Leonova <tanja.v.leonova@gmail.com>
2020-05-26 18:35:16 +0200

    en hög av kokosnötter, collision med player och bounce

---
+++ Unity-project/Assets/Scripts/BounceOnCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceOnCollision : MonoBehaviour
+{
+    [SerializeField] private GroundCheck groundCheck;
+    public float jumpForce = 5f;
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && groundCheck.isGrounded)
+        {
+            rb.constraints = RigidbodyConstraints.None;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            groundCheck.isGrounded = false;          
+        }        
+    }
+
+}
---
+++ Unity-project/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public bool isGrounded = true;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
